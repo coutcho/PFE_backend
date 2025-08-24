6b4ded6f702ecce7dba574120969ab65bd1c0468
Rayan Adjina <rayan.adjina2002@gmail.com>
2025-08-24 20:12:18 +0100

    fix 1000th

--- supabase.js
+++ supabase.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseServiceKey);
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+export default supabase;
